7e7a5981364d6326aec34251ab6f3a4c97dd2f0f
John Ndungu <johnwakaba@gmail.com>
2025-08-12 20:02:34 -0700

    updated app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          John's App Version 1 Learn React
         </a>
       </header>
     </div>
